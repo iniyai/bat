4bb3cbb5549370a1326383bed5db8784a15c27a8
Shashikiran Prasannamurthy <shasrp@oath.com>
2019-06-28 17:49:36 -0700

    more code on histogram

--- histogram_cmd.go
+++ histogram_cmd.go
@@ -2,14 +2,16 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"io"
+	tmap "github.com/emirpasic/gods/maps/treemap"
+	"strconv"
 )
 
 type HistogramCommand struct {
-	span            *int
+	bins            *int
 	horizontalChart *bool
 	maxHeight *int
-//	floatMode *bool
 	cmdOptions      *flag.FlagSet
 }
 
@@ -23,16 +25,74 @@ func (HistogramCommand) Desc() string {
 
 func (hc *HistogramCommand) Init() {
 	hc.cmdOptions = flag.NewFlagSet(hc.Name(), flag.ExitOnError)
-	hc.span = hc.cmdOptions.Int("span", 5, "Histogram bucket size")
+	hc.bins = hc.cmdOptions.Int("bins", 10, "Number of Histogram buckets")
 	hc.horizontalChart = hc.cmdOptions.Bool("hc", true, "Horizontal chart orientation")
 	hc.maxHeight = hc.cmdOptions.Int("maxHeight", 50, "Maximum height of histograms")
 }
 
-func (HistogramCommand) Interact(args []string, stdin io.Reader, stdout io.Writer, stderr io.Writer) error {
-	panic("implement me")
+func (hc *HistogramCommand) Interact(args []string, stdin io.Reader, stdout io.Writer, stderr io.Writer) error {
+	bInOut, _ := ToBuffered(stdin, stdout, stderr)
+	fmap := tmap.NewWithIntComparator()
+	for {
+		line, err := bInOut.Reader.ReadString('\n')
+		if err != nil {
+			break
+		} else {
+			line = line[:len(line)-1]
+			value, err := strconv.Atoi(line)
+			if err != nil {
+				continue
+			}
+			freq, found := fmap.Get(value)
+			if found {
+				fmap.Put(value, freq.(int) + 1)
+			} else {
+				fmap.Put(value, 1)
+			}
+		}
+	}
+
+	if fmap.Empty() {
+		return nil
+	}
+
+	min, _ := fmap.Min()
+	max, _ := fmap.Max()
+
+	// Size and Span
+	hGram := Histogram{bins:*hc.bins, max:max.(int), min:min.(int), hMap:make(map[int]int)}
+	fmap.Each(hGram.add)
+	fmt.Println(hGram.hMap)
+	fmt.Println(fmap)
+
+	return nil
 }
 
 func (hc *HistogramCommand) Help(stderr io.Writer) {
 	hc.cmdOptions.SetOutput(stderr)
 	hc.cmdOptions.Usage()
 }
+
+type Histogram struct {
+	bins, min, max int
+	hMap map[int]int
+}
+
+func (hGram *Histogram) size()  int {
+	return hGram.max - hGram.min + 1
+}
+
+func (hGram *Histogram) span() int {
+	return hGram.size() / hGram.bins
+}
+
+func (hGram *Histogram) findBucket(score int) int {
+	return score /  hGram.span()
+}
+
+func (hGram *Histogram) add(score, freq interface{})  {
+	hGram.hMap[hGram.findBucket(score.(int))] += freq.(int)
+}
+
+
+
